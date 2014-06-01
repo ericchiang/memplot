a729206421e89cb7b9de9c2732d31879a28b2897
ericchiang <eric.chiang.m@gmail.com>
2014-06-01 14:22:59 -0400

    version bumped for pypi upload

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="memplot",
-    version="0.1.0",
+    version="0.1.1",
     author="Eric Chiang",
     author_email="eric@yhathq.com",
     url="https://github.com/EricChiang/memplot",
